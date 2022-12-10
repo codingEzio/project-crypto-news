68ada852b4e6a868e2ae2fcabc38e9dc4b79522c
!Mensch <assassinste@gmail.com>
2022-12-10 17:25:24 +0800

    dev: Enable Bootstrap for components to use

--- src/main.js
+++ src/main.js
@@ -1,16 +1,22 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VueBootStrap from 'bootstrap-vue';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 import App from './App.vue';
 import GlobalHome from './components/screens/GlobalHome';
 import NewsItem from './components/screens/NewsItem';
 
 Vue.use(VueRouter);
+Vue.use(VueBootStrap);
+
 Vue.config.productionTip = false;
 
 const router = new VueRouter({
   routes: [
-    { path: '/', component: GlobalHome },
+    { path: '/home', component: GlobalHome, alias: '/' },
     { path: '/news', component: NewsItem },
   ],
 });
