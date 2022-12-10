d83754fac0eb6e2c6b93c45bb4aefac9a61e6db1
!Mensch <assassinste@gmail.com>
2022-12-10 20:51:40 +0800

    dev: Mockup for homepage jumbotron (big title and text)

--- src/components/screens/GlobalHome.vue
+++ src/components/screens/GlobalHome.vue
@@ -1,6 +1,22 @@
 <template>
   <b-container>
-    <p>Home here</p>
+    <b-jumbotron bg-variant="transparent" text-variant="white">
+      <template slot="header">
+        <span class="d-none d-md-block"> Crypto Conversion </span>
+      </template>
+
+      <template slot="lead">
+        <p class="jumbotron-lead">
+          Compare the latest prices for cryptocurrencies from
+          <b-link
+            class="text-secondary"
+            href="https://min-api.cryptocompare.com/"
+          >
+            CryptoCurrency API
+          </b-link>
+        </p>
+      </template>
+    </b-jumbotron>
   </b-container>
 </template>
 
@@ -9,3 +25,9 @@ export default {
   name: 'GlobalHome',
 };
 </script>
+
+<style scoped>
+.jumbotron-lead {
+  max-width: 600px;
+}
+</style>
