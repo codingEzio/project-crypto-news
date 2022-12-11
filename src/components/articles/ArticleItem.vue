fc5912c67559e1c8144aafe518a189c411ebc616
!Mensch <assassinste@gmail.com>
2022-12-11 11:52:22 +0800

    dev: News items mockup

---
+++ src/components/articles/ArticleItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-card
+    bg-variant="card"
+    border-variant="transparent"
+    title="Article title"
+    img-src="https://placekitten.com/g/300/300"
+    img-fluid
+    img-alt="Placeholder image"
+    img-top
+  >
+    <p class="card-text">Article Summary</p>
+
+    <small class="text-muted">Updated 3 minutes ago</small>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: 'ArticleItem',
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/articles/ArticleList.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-row>
+    <b-col lg="4" sm="6" xs="12" class="mb-4">
+      <ArticleItem />
+    </b-col>
+    <b-col lg="4" sm="6" xs="12" class="mb-4">
+      <ArticleItem />
+    </b-col>
+    <b-col lg="4" sm="6" xs="12" class="mb-4">
+      <ArticleItem />
+    </b-col>
+    <b-col lg="4" sm="6" xs="12" class="mb-4">
+      <ArticleItem />
+    </b-col>
+    <b-col lg="4" sm="6" xs="12" class="mb-4">
+      <ArticleItem />
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+import ArticleItem from '@/components/articles/ArticleItem';
+
+export default {
+  name: 'ArticleList',
+  components: {
+    ArticleItem,
+  },
+};
+</script>
+
+<style scoped></style>
