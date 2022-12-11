ae1543c9150fcb12c4cd1bff4dcf604c4be87cf7
!Mensch <assassinste@gmail.com>
2022-12-11 11:52:48 +0800

    dev: Integrate artile-related components into /news page

--- src/components/screens/NewsItem.vue
+++ src/components/screens/NewsItem.vue
@@ -1,11 +1,29 @@
 <template>
   <b-container>
-    <p>News here</p>
+    <b-jumbotron bg-variant="transparent" text-variant="white">
+      <template slot="header">
+        <span class="d-none d-md-block">Crypto News</span>
+      </template>
+
+      <template slot="lead">
+        <p class="jumbotron-lead">
+          Latest articles from the world of cryptocurrencies. Read what is
+          happening with <em>Bitcoin</em>, <em>Ethereum</em> and other altcoins.
+        </p>
+      </template>
+    </b-jumbotron>
+
+    <ArticleList />
   </b-container>
 </template>
 
 <script>
+import ArticleList from '@/components/articles/ArticleList';
+
 export default {
   name: 'NewsItem',
+  components: {
+    ArticleList,
+  },
 };
 </script>
