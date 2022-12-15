9a2963e6bbc91d7bb936a39f9355c1fb2ac6fb45
!Mensch <assassinste@gmail.com>
2022-12-15 09:52:57 +0800

    revert: Back to hardcoded URL (#TODO fix this)

--- src/api/news.js
+++ src/api/news.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
-import API_PREFIX from '../util/constants';
 
 // No API key is required for fetching the news data
 // this can be directly parsed in components to use (and display)
 export function getLatestNews() {
-  return axios.get(API_PREFIX + '/data/v2/news/', {
+  return axios.get('https://min-api.cryptocompare.com/data/v2/news/', {
     params: {
       lang: 'EN',
       extraParams: 'crypto-news-vue',
