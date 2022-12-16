74f166df9ce5719e9a16925a005cff4a9034c81e
!Mensch <assassinste@gmail.com>
2022-12-16 14:59:09 +0800

    feat: Clickable article title (with styles revamped)

--- src/components/articles/ArticleItem.vue
+++ src/components/articles/ArticleItem.vue
@@ -1,20 +1,30 @@
 <template>
-  <b-card
-    bg-variant="card"
-    border-variant="transparent"
-    :title="title"
-    :img-src="img"
-    img-fluid
-    img-alt="Placeholder image"
-    img-top
-  >
-    <p class="card-text">
-      {{ body }}
-    </p>
+  <b-card no-body bg-variant="card" border-variant="transparent">
+    <a
+      :href="url"
+      target="_blank"
+      rel="noreferrer noopener"
+      class="card-img-container"
+    >
+      <b-img :src="img" :alt="title" class="card-img card-img-top" />
+    </a>
 
-    <small class="text-muted">
-      Updated {{ published }} by {{ publisher }}
-    </small>
+    <b-card-body>
+      <a
+        :href="url"
+        target="_blank"
+        rel="noreferrer noopener"
+        class="text-light"
+      >
+        <h4 class="card-title" v-html="title" />
+      </a>
+
+      <p class="card-text" v-html="body" />
+
+      <small class="text-muted">
+        Updated {{ published }} by {{ publisher }}
+      </small>
+    </b-card-body>
   </b-card>
 </template>
 
@@ -46,7 +56,38 @@ export default {
   display: inline-block;
 }
 
+.card-title,
 .card-text {
-  word-break: break-all;
+  word-break: break-word;
+}
+
+.card-text {
+  text-align: justify;
+  text-align-last: left;
+  text-justify: distribute;
+
+  hyphens: auto;
+}
+
+.card-img-container {
+  display: block;
+
+  position: relative;
+  width: 100%;
+  height: 0;
+
+  padding-top: 100%;
+
+  background-color: #282b2e;
+}
+
+.card-img {
+  position: absolute;
+
+  top: 0;
+  left: 0;
+  width: 100%;
+
+  filter: grayscale(50%) saturate(80%);
 }
 </style>
