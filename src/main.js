1e705fca759264fb039927115c62405efb0683f0
!Mensch <assassinste@gmail.com>
2022-12-16 15:09:55 +0800

    chore: Add and configure dependency for animation library

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import VueBootStrap from 'bootstrap-vue';
+import aos from 'aos';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'aos/dist/aos.css';
 
 import App from './App.vue';
 import GlobalHome from './components/screens/GlobalHome';
@@ -23,6 +25,10 @@ const router = new VueRouter({
 });
 
 new Vue({
+  created() {
+    aos.init();
+  },
+
   store,
   router,
   render: h => h(App),
