0d3ebc94282cd9dc2b823e18cb2b81976683270d
!Mensch <assassinste@gmail.com>
2022-12-18 20:18:13 +0800

    fix: Generated code output path when deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
-  baseUrl:
+  publicPath:
     process.env.NODE_ENV === 'production' ? '/project-crypto-news/' : '/',
 });
