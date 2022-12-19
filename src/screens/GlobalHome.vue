0c3397995bbcd69cb902966ef7fb85f1c23e2124
!Mensch <assassinste@gmail.com>
2022-12-19 14:58:38 +0800

    refactor: Switch to SCSS syntax

--- src/screens/GlobalHome.vue
+++ src/screens/GlobalHome.vue
@@ -1,20 +1,18 @@
 <template>
   <b-container>
-    <b-jumbotron bg-variant="transparent" text-variant="white">
+    <b-jumbotron bg-variant="dark" text-variant="white">
       <template slot="header">
         <span class="d-none d-md-block"> Crypto Conversion </span>
       </template>
 
       <template slot="lead">
-        <p class="jumbotron-lead">
-          Compare the latest prices for cryptocurrencies from
-          <b-link
-            class="text-secondary"
-            href="https://min-api.cryptocompare.com/"
-          >
-            CryptoCurrency API
-          </b-link>
-        </p>
+        Compare the latest prices for cryptocurrencies from
+        <b-link
+          class="text-secondary"
+          href="https://min-api.cryptocompare.com/"
+        >
+          CryptoCurrency API
+        </b-link>
       </template>
 
       <PriceConverter class="mt-5 converter" />
@@ -37,46 +35,44 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .jumbotron {
   position: relative;
-}
 
-.jumbotron::before {
-  position: absolute;
-  left: 15px;
-  top: 0;
+  &::before {
+    position: absolute;
+    left: 15px;
+    top: 0;
 
-  background-image: url('../../public/images/map.png');
-  background-repeat: no-repeat;
-  background-size: contain;
-  background-position: center left;
+    background-image: url('../../public/images/map.png');
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center left;
 
-  width: 450px;
-  height: 300px;
+    width: 450px;
+    height: 300px;
 
-  content: '';
-}
+    content: '';
 
-.jumbotron-lead {
-  max-width: 600px;
-}
+    @media screen and (max-width: 480px) {
+      left: 0;
 
-@media screen and (max-width: 480px) {
-  .jumbotron::before {
-    left: 0;
+      width: 100%;
+      height: 200px;
+    }
+  }
 
-    width: 100%;
-    height: 200px;
+  .display-3,
+  .lead {
+    max-width: 600px;
+
+    @media screen and (max-width: 820px) {
+      margin: 0 auto;
+      text-align: center;
+    }
   }
 }
-
 @media screen and (max-width: 820px) {
-  .jumbotron .display-3,
-  .jumbotron .lead {
-    text-align: center;
-  }
-
   .converter {
     margin: 0 auto;
   }
